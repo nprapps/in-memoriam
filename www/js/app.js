23f2702204c3d505ae21b3606e2999d532e5c303
Alyson Hurt <ahurt@npr.org>
2012-12-04 16:56:14 -0500

    template customizations, slide styling

--- www/js/app.js
+++ www/js/app.js
@@ -2,6 +2,9 @@ $(function() {
 	/* VARS */
 	var slideshow_data = [];
 	var slide_output = '';
+	
+	/* ELEMENTS */
+	var $s = $('#slideshow');
 
 	/* LOAD SLIDESHOW DATA FROM EXTERNAL JSON */
 	function load_slideshow_data() {
@@ -11,34 +14,19 @@ $(function() {
 				var slide_info = '';
 				
 				slide_info += '<div id="slide' + k + '" class="slideshow_item" style="background-image: url(http://media.npr.org/assets/music/specials/memoriam2011/images/' + v["image"] + ');">';
-//				slide_info += '<div id="slide' + k + '" class="slideshow_item">';
 				slide_info += '<div class="slide_description">';
 				slide_info += '<h2>' + v["artist_first_name"] + ' ' + v["artist_last_name"] + '</h2>';
 				slide_info += '<p class="dates">' + v["dob"] + '-' + v["dod"] + '</p>';
 				slide_info += '<p class="desc">' + v["known_as"] + '</p>';
 				if (v["full_obit"].length > 2) {
-					slide_info += '<p class="npr-obit"><a href="http://www.npr.org/templates/story/story.php?storyId=' + v["full_obit"] + '" target="_blank">NPR Remembrance</a></p>';
+					slide_info += '<p class="npr-obit">&gt; <a href="http://www.npr.org/templates/story/story.php?storyId=' + v["full_obit"] + '" target="_blank">NPR Remembrance</a></p>';
 				}
 				if (v["artist_page_id"].length > 2) {
-					slide_info += '<p class="npr-artist"><a href="http://www.npr.org/artists/' + v["artist_page_id"] + '" target="_blank">NPR Artist Page</a></p>';
+					slide_info += '<p class="npr-artist">&gt; <a href="http://www.npr.org/artists/' + v["artist_page_id"] + '" target="_blank">NPR Artist Page</a></p>';
 				}
 				slide_info += '<p class="now-playing">Now Playing: ' + v["song_name"] + '</p>';
 				slide_info += '<p class="photo-credit">Photo: ' + v["photo_credit"] + '</p>';
 				
-// [{"sort": ["Arroyo", "Joe"], 
-// "artist_first_name": "Joe", 
-// "image": "joearroyo.jpg", 
-// "known_as": "Salsa Icon", 
-// "cue_start": 0, 
-// "full_obit": "138707179", 
-// "cue_end": 20, 
-// "artist_last_name": "Arroyo", 
-// "artist_page_id": "", 
-// "photo_credit": "Courtesy of the artist", 
-// "dob": "11/1/1955", 
-// "song_name": "Joe Arroyo, \u201cEn Barranquilla Me Quedo\u201d", 
-// "dod": "7/26/2011"}
-
 				slide_info += '</div>'; // end .slide_description
 				slide_info += '</div>'; // end .slideshow_item
 				
@@ -46,7 +34,7 @@ $(function() {
 				
 			});
 			
-			$('body').append('<div id="slideshow_wrap">' + slide_output + '</div>');
+			$s.append('<div id="slideshow_wrap">' + slide_output + '</div>');
 		});
 	}
 	
