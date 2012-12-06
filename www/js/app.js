9fa7e80e65938cc76eea1a0f9485324f7aa6768d
Alyson Hurt <ahurt@npr.org>
2012-12-06 09:20:20 -0500

    made slide nav clicky. need bigger click targets

--- www/js/app.js
+++ www/js/app.js
@@ -80,6 +80,11 @@ $(document).ready(function() {
 			
 			$s.append('<div id="slideshow-wrap">' + slide_output + '</div>');
 			$slide_nav.append(audio_output);
+			
+			$slide_nav.find('.slide-nav-item').click( function() {
+				var id = $(this).attr('data-id');
+				$player.jPlayer("play", slideshow_data[id]['cue_start']);
+			});
 		});
 	}
 	
